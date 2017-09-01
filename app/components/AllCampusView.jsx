20a948fa58b174dd1edaef72a7abfafd5c409bce
Jason <Jason@Jasons-MacBook-Pro-2.local>
2017-09-01 19:07:47 -0400

    this is my submission for friday

---
+++ app/components/AllCampusView.jsx
@@ -0,0 +1,94 @@
+//THis is where we are creatign the box that shows the all cmapuses. THe campuses are being rendered by a dummy component 
+
+import React, { Component } from 'react';
+import axios from 'axios';
+import DumbCampusRender from './DumbCampusRender';
+import StudentList from './StudentList';
+
+export default class OldRoot extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      campuses: [],
+      selectedCampus:{},
+      newCampus: ""
+    }
+    this.selectCampus=this.selectCampus.bind(this)
+    this.handleChange=this.handleChange.bind(this)
+    this.handleSubmit=this.handleSubmit.bind(this)
+    this.handleClick=this.handleClick.bind(this)
+  }
+
+  componentDidMount() {
+    axios.get('/api/campuses')
+    .then(res=>res.data)
+    .then(campuses=>this.setState({campuses}))
+    .catch(console.log)
+  }
+
+selectCampus(campusId){
+  axios.get(`/api/campuses/${campusId}`)
+  .then(res=>res.data)
+  .then(campus=>this.setState({selectedCampus: campus}))
+}
+
+addCampus(){
+  axios.post('/campuses')
+}
+handleSubmit(ev){
+  // console.log("NEW CAMPUS", this.state.newCampus)
+  ev.preventDefault()
+  axios.post('api/campuses',{
+    name:this.state.newCampus})
+  .then(response=>{
+    console.log("SAVE SUCESSFUL", response)
+    console.log("NEW CAMPUSES", this.state)
+    // this.setState({newCampus: ""})
+    this.setState({campuses:[...this.state.campuses, response.data],
+    newCampus:""})
+  })
+  .catch(console.log)
+
+}
+
+handleChange(ev){
+  // console.log(this.state.newCampus)
+  this.setState({newCampus: ev.target.value})
+  // console.log(`INSIDE handleChange ${ev.target.value}`)
+}
+
+handleClick(campusId){
+  console.log("Clicked")
+  axios.delete(`api/campuses/${campusId}`)
+  .then(response=>axios.get('/api/campuses')
+  )
+  .then(response=>response.data)
+  .then(campuses=>this.setState({campuses}))
+  // .then(campuses=>this.setState({campuses}))
+  .catch(console.log)
+}
+
+  render() {
+    // console.log("ALL CAMPUS STATE", this.state)
+  //  console.log('selected Campus', this.state.selectedCampus.name)
+    return (
+      <div id="main">
+      
+
+      <form onSubmit={this.handleSubmit}>
+      Add a Campus {"\n"}
+      <input onChange = {this.handleChange}type="text" value={this.state.newCampus} ></input>
+      <input type="submit"></input>
+      </form>
+      
+        <DumbCampusRender campuses={this.state.campuses} 
+                          selectCampus={this.selectCampus}
+                          handleClick={this.handleClick}/>
+       { this.state.selectedCampus.id?
+         <StudentList  campus={this.state.selectedCampus}/>
+         :<div></div>
+        }
+      </div>
+    )
+  }
+}
--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -1,76 +1,41 @@
+//OUR HOME PAGE
 import React, { Component } from 'react';
-import axios from 'axios';
-import AllCampuses from './AllCampuses';
-import StudentList from './StudentList';
+import DumbCampusRender from './DumbCampusRender';
+import AllCampusView from './AllCampusView.jsx';
+import {HashRouter as Router, Route, Link, IndexRoute, hashHistory, browserHistory} from 'react-router-dom';
+import AllStudentsView from './AllStudentsView'
+import SingleStudentView from './SingleStudentView'
 
 export default class Root extends Component {
-  constructor(props) {
-    super(props)
-    this.state={
-      campuses: [],
-      selectedCampus:{},
-      newCampus:{}
-    }
-    this.selectCampus=this.selectCampus.bind(this)
-    this.handleChange=this.handleChange.bind(this)
-    this.handleSubmit=this.handleSubmit.bind(this)
-  }
-
-  componentDidMount() {
-    axios.get('/api/campuses')
-    .then(res=>res.data)
-    .then(campuses=>this.setState({campuses}))
-    .catch(console.log)
-  }
-
-selectCampus(campusId){
-  axios.get(`/api/campuses/${campusId}`)
-  .then(res=>res.data)
-  .then(campus=>this.setState({selectedCampus: campus}))
-}
-
-addCampus(){
-  axios.post('/campuses')
-}
-handleSubmit(ev){
-  // console.log("NEW CAMPUS", this.state.newCampus)
-  axios.post('api/campuses',{
-    name:this.state.newCampus})
-  .then(response=>{
-    console.log("SAVE SUCESSFUL", response)
-    console.log("NEW CAMPUSES", this.state)
-    // this.setState({newCampus: ""})
-    this.setState({campuses:[...this.state.campuses, response.data]})
-  })
-  .catch(console.log)
-
-}
-
-handleChange(ev){
-  // console.log(this.state.newCampus)
-  this.setState({newCampus: ev.target.value})
-  // console.log(`INSIDE handleChange ${ev.target.value}`)
-}
 
   render() {
-    // console.log("new campus", this.state.newCampus)
-  //  console.log('selected Campus', this.state.selectedCampus.name)
     return (
+      <Router history={hashHistory}>
       <div id="main">
       <h1>Interplanetary Academy of JavaScript</h1>
-
-      <form onSubmit={this.handleSubmit}>
-      Add a Campus {"\n"}
-      <input onChange = {this.handleChange}type="text" ></input>
-      <input type="submit"></input>
-      </form>
-      
-        <AllCampuses campuses={this.state.campuses} selectCampus={this.selectCampus}/>
-       { this.state.selectedCampus.id?
-         <StudentList  campus={this.state.selectedCampus}/>
-         :<div></div>
-        }
+        <Container />
+        <Route exact path='/' component={AllCampusView}/>
+        <Route exact path="/students" component={AllStudentsView} />
+        <Route  exact path='/students/:id' component={SingleStudentView} />
+        {/*}
+        <Route exact path= '/campuses/:id' component={newStudentList} />
+    */}
       </div>
+      </Router>
     )
   }
 }
+
+
+
+const Nav = () => (
+  <div>
+    <button><Link to="/">Campus View</Link></button>&nbsp;
+    <button><Link to="/students">Student View </Link></button>
+  </div>
+)
+
+const Container = (props) => (<div>
+<Nav />
+{props.children}
+</div>)
